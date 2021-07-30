bcbc9e3e982f95d720ef370fb9cb0eafe3c491a8
DevSmartChain <88134314+DevSmartChain@users.noreply.github.com>
2021-07-30 15:14:47 +0400

    Update hooks.ts

--- src/state/lists/hooks.ts
+++ src/state/lists/hooks.ts
@@ -2,7 +2,7 @@ import { ChainId, Token } from '@kuniswap/sdk'
 import { Tags, TokenInfo, TokenList } from '@uniswap/token-lists'
 
 import { AppState } from '../index'
-import DEFAULT_TOKEN_LIST from '@kukuswap/default-token-list'
+import DEFAULT_TOKEN_LIST from '../../constants/token-lists/kuniswap.json'
 import { UNSUPPORTED_LIST_URLS } from './../../constants/lists'
 import UNSUPPORTED_TOKEN_LIST from '../../constants/token-lists/sushiswap-v2-unsupported.tokenlist.json'
 import sortByListPriority from 'utils/listSort'
